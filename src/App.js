ef4ec75e9eb69d94aff443529b32487eed3c0b1e
amadeus211 <malyuk2004.mail.ru@gmail.com>
2024-05-15 19:50:14 +0300

    1234

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
-import "./App.css";
 import PaletteUIColor from "./components/paletteIcons/paletteUIColor";
+import Palette from "./pallete.json"; // Імпортуємо палетки
 
 function App() {
   return (
-
     <div className="app-container">
       <div className="content">
-        <div className="title">
-          Flat UI Colors 2
-        </div>
-
+        <div className="title">Flat UI Colors 2</div>
         <div className="palette-grid">
-          <NavLink to="/materialUIColor">
-            <PaletteUIColor paletteName="Material UI Colors" />
-          </NavLink>
-          <NavLink to="/flatUIcolor">
-            <PaletteUIColor paletteName="Flat UI Colors v1" />
-          </NavLink>
-          <NavLink to="/dutchUIcolor">
-            <PaletteUIColor paletteName="Flat UI Colors Dutch" />
-          </NavLink>
-          <NavLink to="/americanUIcolor">
-            <PaletteUIColor paletteName="Flat UI Colors American" />
-          </NavLink>
-          <NavLink to="/aussieUIcolor">
-            <PaletteUIColor paletteName="Flat UI Colors Aussie" />
-          </NavLink>
-          <NavLink to="/britishUIcolor">
-            <PaletteUIColor paletteName="Flat UI Colors British" />
-          </NavLink>
-          <NavLink to="/spanishUIcolor">
-            <PaletteUIColor paletteName="Flat UI Colors Spanish" />
-          </NavLink>
-          <NavLink to="/indianUIcolor">
-            <PaletteUIColor paletteName="Flat UI Colors Indian" />
-          </NavLink>
-          <NavLink to="/frenchUIcolor">
-            <PaletteUIColor paletteName="Flat UI Colors French" />
-          </NavLink>
+          {Palette.map((palette, index) => (
+            <NavLink key={index} to={`/${palette.id}`}>
+              <PaletteUIColor paletteName={palette.paletteName} />
+            </NavLink>
+          ))}
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,23 +5,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import PalettePage from "./components/palettePages/palettePage";
-
+import Palette from "./pallete.json"; // Імпортуємо палетки
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route exact path="/" element={<App />} />
-        <Route path="/materialUIcolor" element={<PalettePage paletteName="Material UI Colors" />} />
-        <Route path="/flatUIcolor" element={<PalettePage paletteName="Flat UI Colors v1" />}/>
-        <Route path="/dutchUIcolor" element={<PalettePage paletteName="Flat UI Colors Dutch" />}/>
-        <Route path="/americanUIcolor" element={<PalettePage paletteName="Flat UI Colors American" />}/>
-        <Route path="/aussieUIcolor" element={<PalettePage paletteName="Flat UI Colors Aussie" />}/>
-        <Route path="/britishUIcolor" element={<PalettePage paletteName="Flat UI Colors British" />}/>
-        <Route path="/spanishUIcolor" element={<PalettePage paletteName="Flat UI Colors Spanish" />}/>
-        <Route path="/indianUIcolor" element={<PalettePage paletteName="Flat UI Colors Indian" />}/>
-        <Route path="/frenchUIcolor" element={<PalettePage paletteName="Flat UI Colors French" />}/>
-
+        {Palette.map((palette, index) => (
+          <Route 
+            key={index} 
+            path={`/${palette.id}`} 
+            element={<PalettePage paletteName={palette.paletteName} />} 
+          />
+        ))}
       </Routes>
     </Router>
   </React.StrictMode>,
